3b04ce2d2ae1a926036c7b2a723c5d8edf15c172
KaiserHua1901 <KaiserHua1901@outlook.com>
2024-04-12 13:37:32 -0700

    PC have some more animation
    - Creating a barrier for up and down of the scene
    - Adding the up and down animation when press space and not
    - stop PC from playing animation and stop the physic process when hit the floor
    - Making a input when pressing space it fly

--- Scenes/Plane.gd
+++ Scenes/Plane.gd
@@ -1,13 +1,48 @@
 extends CharacterBody2D
-const GRAVITY: float = 120.0
+const GRAVITY: float = 800.0
+const FLY: float = -500.0
+var trigger:bool = false
+@onready var CharacterSprite: AnimatedSprite2D = $Sprite
+@onready var animation_player:AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
+	# Creating a gravity 
+	#if Input.is_action_just_pressed("fly") == false and velocity.y <= 0:
+		#animation_player.queue("idle")
+
+		
+	fly()
 	
 	move_and_slide()
+	
+	if is_on_floor() == true:
+		Death()
+	# Stop Animation from doing and stop the physic process when hitting the floor 
+
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true and trigger:
+		velocity.y = FLY
+		animation_player.play("fly")
+		trigger = false
+		print(trigger)
+		
+	elif not trigger:
+		animation_player.queue("down")
+		trigger = true
+		print(trigger)
+		
+
+func Death():
+	CharacterSprite.stop()
+	set_physics_process(false)
+
+
+
+
